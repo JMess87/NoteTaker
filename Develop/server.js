7dd1b4a4309a89643bb64af46144fe20f3195022
Jennilee Messenger <jennilee.messenger@gmail.com>
2023-02-02 18:45:05 -0500

    adding routes folder and server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 const PORT = 3001;
 
+// middleware
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+// route for home page
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/routes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/routes.html'))
+// route for notes page
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+
+
+// reference from GW Mini project Module 11- wildcard route
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/404.html'))
+);
+
+
+//code provided by GW - should appear after installing npm and running node server
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
+
